fc75d442571ca5ac18f68c579633ae1b6c069b51
itzniel <w0817788@myscc.ca>
2023-04-06 14:00:16 -0400

    Added the Category Controller

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DanBookStore.DataAccess.Data;
+using DanBooks.DataAccess.Repository.IRepository;
+using DanBooks.Models;
+
+namespace NiharsBookStore.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                //this is for create
+                return View(category);
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category); //remember
+
+        }
+    }
+}
